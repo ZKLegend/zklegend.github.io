dac264703f15db348bb8bea5f09ef25cf3f523fa
ZKLegend <lekinhanh91@gmail.com>
2022-11-22 15:23:20 +0700

    Update category slide item

--- src/components/CategorySlide/index.jsx
+++ src/components/CategorySlide/index.jsx
@@ -1,26 +1,36 @@
 import React from "react";
+import axios from 'axios';
+import { useState, useEffect } from "react";
 import './style.css';
 
+const CategorySlideItem = (image) => {
+    return (
+        <div className="category-slide-item">
+            Category Slide Image
+        </div>
+    )
+}
+
 const CategorySlide = () => {
+    const [itemList, setItemList] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://637c33cc72f3ce38ea9ce6a2.mockapi.io/AnimeList")
+        .then((res) => {
+            setItemList([...res.data]); 
+            console.log(res.data);  
+        })
+        console.log(itemList);
+    }, []);
+
     return (
         <div className="category-slide">
             <h2 className="category-name">Category 1</h2>
             <div className="category-slide-container">
-                <div className="category-slide-item">
+                <CategorySlideItem />
+                {/* <div className="category-slide-item">
                     Category Slide Image 
-                </div>
-                <div className="category-slide-item">
-                    Category Slide Image
-                </div>
-                <div className="category-slide-item">
-                    Category Slide Image 
-                </div>
-                <div className="category-slide-item">
-                    Category Slide Image
-                </div>
-                <div className="category-slide-item">
-                    Category Slide Image
-                </div>
+                </div> */}
             </div>
         </div>
     )
