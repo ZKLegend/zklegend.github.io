30d31493e73cb1bfe0a4e8cc10057942fcc48c7e
ZKLegend <lekinhanh91@gmail.com>
2022-11-22 17:03:36 +0700

    Update HomePage

--- src/components/CategorySlide/index.jsx
+++ src/components/CategorySlide/index.jsx
@@ -1,39 +1,35 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useState, useEffect } from "react";
-import './style.css';
+import "./style.css";
 
-const CategorySlideItem = (image) => {
-    return (
-        <div className="category-slide-item">
-            Category Slide Image
-        </div>
-    )
-}
+const CategorySlideItem = ({ image }) => {
+  return <div className="category-slide-item">{image}</div>;
+};
 
-const CategorySlide = () => {
-    const [itemList, setItemList] = useState([]);
+const CategorySlide = ({ name }) => {
+  const [itemList, setItemList] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://637c33cc72f3ce38ea9ce6a2.mockapi.io/AnimeList")
-        .then((res) => {
-            setItemList([...res.data]); 
-            console.log(res.data);  
-        })
-        console.log(itemList);
-    }, []);
-
-    return (
-        <div className="category-slide">
-            <h2 className="category-name">Category 1</h2>
-            <div className="category-slide-container">
-                <CategorySlideItem />
-                {/* <div className="category-slide-item">
-                    Category Slide Image 
-                </div> */}
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    axios
+      .get("https://637c33cc72f3ce38ea9ce6a2.mockapi.io/AnimeList")
+      .then((res) => {
+        setItemList([...res.data]);
+        // console.log("ItemList:", itemList);
+        // Đoạn log này chưa hiểu sao nó lại chạy hai lần 
+      });
+  }, []);
+  // console.log("ItemList:", itemList);
+  return (
+    <div className="category-slide">
+      <h2 className="category-name">{name}</h2>
+      <div className="category-slide-container">
+        {itemList.map((element) => (
+          <CategorySlideItem image={element.image} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CategorySlide;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import BigPoster from "../../components/BigPoster";
 import CategorySlide from "../../components/CategorySlide";
 
-
 const HomePage = () => {
-    return(
-        <div className="home-page">
-            <BigPoster />
-            <CategorySlide />
-        </div>
-    )
-}
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://637c33cc72f3ce38ea9ce6a2.mockapi.io/AnimeList")
+      .then((res) => {
+        console.log("Res:", res);
+        console.log(res.data);
+        setCategoryList([...res.data]);
+      });
+  }, []);
+
+//   console.log("Category:", categoryList);
+
+  return (
+    <div className="home-page">
+      <BigPoster />
+      {categoryList.map((element) => (
+        <CategorySlide name={element.name} />
+      ))}
+      {/* Đoạn trên đang bị một lỗi là mỗi khi render ra 1 cái CategorySlide thì nó sẽ gửi API request, vậy có ok không? */}
+    </div>
+  );
+};
 
 export default HomePage;
