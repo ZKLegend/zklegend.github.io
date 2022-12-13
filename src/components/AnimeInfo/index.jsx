789039dfb284647c579fffaf5e381e0f9d7f5e30
ZKLegend <lekinhanh91@gmail.com>
2022-12-13 17:45:30 +0700

    Final 1

--- src/components/AnimeInfo/index.jsx
+++ src/components/AnimeInfo/index.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { Row, Col, Descriptions, Typography } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const AnimeInfo = (props) => {
   const [animeDetail, setAnimeDetail] = useState({});
   const [animeGenre, setAnimeGenre] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getAnimeDetail = async () => {
+      setIsLoading(true);
       const response = await axios.get(
         `https://gogoanime.consumet.org/anime-details/${props.animeId}`
       );
       setAnimeDetail({ ...response.data });
       setAnimeGenre([...response.data.genres]);
+      setIsLoading(false);
     };
     getAnimeDetail();
   }, []);
 
   return (
     <div>
-      {props.params.animeId === props.animeId ? (
+      {!isLoading && props.params.animeId === props.animeId ? (
         <Row>
           <Col span={24}>
             <Descriptions
@@ -58,25 +61,31 @@ const AnimeInfo = (props) => {
       ) : (
         <Row>
           <Col span={8}>
-            <Descriptions style={{ width: "200px" }} column={1}>
-              <Descriptions.Item label="Type">
-                {animeDetail.type}
-              </Descriptions.Item>
-              <Descriptions.Item label="Release Date">
-                {animeDetail.releasedDate}
-              </Descriptions.Item>
-              <Descriptions.Item label="Status">
-                {animeDetail.status}
-              </Descriptions.Item>
-              <Descriptions.Item label="Genre">
-                {animeGenre.map((element) => (
-                  <>
-                    {element}
-                    <br />
-                  </>
-                ))}
-              </Descriptions.Item>
-            </Descriptions>
+            {isLoading ? (
+              <h1>
+                <LoadingOutlined />
+              </h1>
+            ) : (
+              <Descriptions style={{ width: "200px" }} column={1}>
+                <Descriptions.Item label="Type">
+                  {animeDetail.type}
+                </Descriptions.Item>
+                <Descriptions.Item label="Release Date">
+                  {animeDetail.releasedDate}
+                </Descriptions.Item>
+                <Descriptions.Item label="Status">
+                  {animeDetail.status}
+                </Descriptions.Item>
+                <Descriptions.Item label="Genre">
+                  {animeGenre.map((element) => (
+                    <>
+                      {element}
+                      <br />
+                    </>
+                  ))}
+                </Descriptions.Item>
+              </Descriptions>
+            )}
           </Col>
         </Row>
       )}
